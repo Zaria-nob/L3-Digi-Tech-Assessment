975c969006c325e097a1cf4269f5169b4fcc00d8
Zaria-nob <zarianobilo@rhs.school.nz>
2024-10-06 17:24:34 +1300

    Add files via upload
    I added a variable for the mole position and added code to move the mole. I also added some comments saying what parts of my code does.
    This is now a fully working whack a mole game.

--- whacAMole.py
+++ whacAMole.py
@@ -3,7 +3,7 @@
 import sys
 import random
 # * imports all functions from that library
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QInputDialog, QMessageBox, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QInputDialog, QMessageBox, QLabel, QVBoxLayout
 from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import *
 
@@ -11,73 +11,99 @@ class Whack_a_mole(QWidget):
     #This function has all the variables in it and initializes the code
     def __init__(self):
         super().__init__()
-        #Creates variables for the rows, columns, and turns
+        #Creates variables for the rows, columns, mole position, and turns
         self.number_of_rows = 3
         self.number_of_cols = 3
-        self.number_of_turns = 0
+        self.score = 0
         self.game_ended = False
-        #creates button for mole
+        #initializes mole_position
+        self.mole_position = (-1,-1) 
     
         self.init_ui()
     
     #Function to set up grid
     def init_ui(self):
-        self.setWindowTitle('Whac-A-Mole')
+        #This creates the name and dimensions of the window
+        self.setWindowTitle('Whack-A-Mole')
+        self.setGeometry(300,200,350,400)
 
+        self.layout = QVBoxLayout()
+
+        #this creates the pop up box for the time limit
+        self.score_label = QLabel(f'Score: {self.score}', self)
+        self.layout.addWidget(self.score_label)
+
+        #This includes the text and min and max
         self.time_limit, ok = QInputDialog.getInt(self, 'Input Dialog', f'Enter the game time in seconds:', min=15, max=60)
-
+        if not ok:
+            #closes the program if input was cancelled
+            self.close()
+    
+        #this starts the timer and times it by 1000 to convert it to seconds.
         QTimer.singleShot(self.time_limit * 1000, self.end_game)
 
+        #creates the grid for the main game
         self.grid = QGridLayout()
         self.setLayout(self.grid)
+        #this creates a set to store the buttons
+        self.buttons = {}
 
-        self.buttons = [[QPushButton('') for _ in range(3)] for _ in range(3)]
-
+        #This creates the buttons for the grid
         for row in range(self.number_of_rows):
             for col in range(self.number_of_cols):
-                button = self.buttons[row][col]
+                button = QPushButton('', self)
                 button.setFixedSize(100,100)
-                #button.clicked.connect(lambda ch, row=row, col=col: self.on_click(row, col))
+                button.clicked.connect(lambda ch, r=row, c=col: self.on_click(r, c))
                 self.grid.addWidget(button, row, col)
-    
+                self.buttons[(row, col)] = button
 
+        self.layout.addLayout(self.grid)
+        self.setLayout(self.layout)
+
+        #move the mole initially
+        self.mole_move
+
+        #create a timer to move the mole every second
+        self.mole_timer = QTimer(self)
+        self.mole_timer.timeout.connect(self.mole_move)
+        self.mole_timer.start(1000)
+
+
+
+    def mole_move(self):
+        #sets original position for the mole
+        if self.mole_position != (-1,-1):
+            self.buttons[self.mole_position].setText('')
+        
+        #makes the mole move randomly between the squares in the grid once clicked.
+        self.mole_position = (random.randint(0, 2), random.randint(0, 2))
+        self.buttons[self.mole_position].setText('Mole!')
+        
+
+
+    #finds out if the clicked position is where the mole is and then adds to the score if it is correct
     def on_click(self, row, col):
-        #print(f'Button at ({row}, {col}) clicked')
-        button = self.sender()
-        self.number_of_turns += 1
-        self.mole_button = self.setGeometry(random.randint())
+        if (row, col) == self.mole_position:
+            self.score += 1
+            #updates score label after you score
+            self.score_label.setText(f'Score: {self.score}')
+            self.buttons[self.mole_position].setText('')
+        else:
+            #Prints in the terminal when the player misses a mole
+            print('you missed the mole')
 
-            
+
 
     def end_game(self):
-        QMessageBox.information(self, 'Game Over',  f'Game Over! You got a score of {self.number_of_turns}')
+        #stops moving the mole
+        self.mole_timer.stop()
+        #shows the final score in a pop up window
+        QMessageBox.information(self, 'Game Over',  f'Game Over! You got a score of {self.score}')
         print('Game Over')
         file = open('file.txt', 'w')
+        #closes file
         file.close()
-
-
-
-
-class Mole(QWidget):
-    def __init__(self):
-        #creates button for mole
-        self.mole = QPushButton()
-
-
-
-    def mole_setup(self):
-        #sets position and dimensions of the mole
-        self.setGeometry(200, 200, 90, 90)
         
-        #makes mole move once clicked
-        self.mole_button.clicked.connect(self.on_click)
-
-
-    def mole_move():
-        #sets original position for the mole
-        #makes the mole move randomly between the squares in the grid once clicked.
-        
-
 
 
 # Makes sure it is running the Main code
